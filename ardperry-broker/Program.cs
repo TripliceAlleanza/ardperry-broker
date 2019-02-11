2200960745c60c3377544daecd1f54e4fb7d4f33
m_inf2.quinta <m_inf2.quinta@C215-026>
2019-02-11 12:22:31 +0100

    ClientCode

--- ardperry-broker/Program.cs
+++ ardperry-broker/Program.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.IO.Ports;
 
 namespace ardperry_broker {
 	class Program {
+		static Socket sendSocket;
+		static SerialPort serial;
+
 		static void Main(string[] args) {
+
+
+			var config = GetConfig();
+
+			sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			serial = new SerialPort(config["SerialPort"]);
+			serial.DataReceived += Serial_DataReceived;
+			sendSocket.Connect(new IPEndPoint(IPAddress.Parse(config["ServerIP"]), int.Parse(config["ServerPort"])));
+			serial.Open();
+
+			
+
+			while (true) {
+				;
+			}
+
+
+			sendSocket.Close();
+		}
+
+		static void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+			
+			string readString = serial.ReadLine();
+			Console.WriteLine("Sending " + readString);
+			sendSocket.Send(Encoding.UTF8.GetBytes($"1#{readString}"));	
+		}
+
+		static Dictionary<string, string> GetConfig() {
+			string text = File.ReadAllText("./config.conf").Replace("\r", "");
+
+			return text.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+			   .Select(part => part.Split('='))
+			   .ToDictionary(split => split[0], split => split[1]);
 		}
 	}
 }
